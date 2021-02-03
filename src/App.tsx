73b9c1c95a22bd62f46461c4588f177e1b9b6fc6
pawfen <sh0n.mishel34@gmail.com>
2021-02-03 22:07:07 +0300

    todo

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Test from './components/Test'
 
+
 function App() {
   return (
     <div className="App">
--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,10 +1,72 @@
 import React from 'react';
+import Container from '@material-ui/core/Container'
 import Button from '@material-ui/core/Button';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import TextField from '@material-ui/core/TextField'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import InboxIcon from '@material-ui/icons/Inbox';
+import ListItemText from '@material-ui/core/ListItemText'
+import { useState } from 'react'
+
+type Todo = {
+  checked: boolean
+  value: string
+}
 
 export default function Test() {
+
+  const [textValue, setTextValue] = useState("")
+  const [notes, setNotes] = useState<Todo[]>([])
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTextValue(e.currentTarget.value)
+  }
+  const handleClick = () => {
+    setNotes([...notes, { checked: false, value: textValue }])
+  }
+  const handleCheckbox = (e: React.ChangeEvent<HTMLInputElement>, checkIndex: number) => {
+    // const newNotes = notes.map((v, i) => ({ ...v, checked: true }))
+    const newNotes: Todo[] = []
+    for (let i = 0; i < notes.length; i++) {
+      if (checkIndex === i) {
+        newNotes.push({...notes[i], checked: ! notes[i].checked })
+      } else {
+        newNotes.push(notes[i])
+      }
+    }
+    setNotes(newNotes)
+    console.log(checkIndex)
+  }
+  // const handleIcon = (e: any) => {
+  //   e.target.color = "primary"
+  // }
+
+  const checkBoxes = notes.map((obj, i) => {
+    return (
+      <ListItem button={false}>
+        <ListItemIcon>
+          <InboxIcon color="primary" />
+        </ListItemIcon>
+        <FormControlLabel control={<Checkbox name="checkedA" checked={obj.checked} onChange={(e) => handleCheckbox(e, i)} />} label={obj.value} disabled={false} />
+      </ListItem>)
+  })
+
   return (
-    <Button variant="contained" color="primary">
-      Hello World
-    </Button>
+    <Container maxWidth="sm">
+      <FormGroup row={true}>
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" onChange={handleChange} value={textValue} />
+        <Button variant="contained" color="secondary" onClick={handleClick}>
+          Add Note
+        </Button>
+      </FormGroup>
+      <List component="nav" aria-label="main mailbox folders">
+        {checkBoxes}
+      </List>
+    </Container>
   );
 }
+
