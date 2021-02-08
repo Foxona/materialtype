230c5bdb9a6342ad7ef7b36381cd316f648e21ec
pawfen <sh0n.mishel34@gmail.com>
2021-02-08 13:40:51 +0300

    New Game Component. Front of game.

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
 import { useState } from 'react'
 
-const arr = [
-    ["X", "X", "O"],
-    ["X", "X", "O"],
-    ["X", "X", "O"]
-]
 
-const printboard = () => {
-    console.log(`Содержимое массива arr: ` + arr)
+
+function Game() {
+    return (<div>
+        
+    </div>)
 }
 
-export default function Game() {
-    const [state, setState] = useState("")
-
-    return (
-        <div>
-            
-        </div>
-    )
+export default function GameRender() {
+    return (<Game />)
 }
-
-printboard()
