b3be354732086d113855f8845aaea52d4c309856
Pawfen <sh0n.mishel34@gmail.com>
2021-02-11 12:55:05 +0300

    9 elems

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -4,9 +4,29 @@ import { useState } from 'react'
 
 
 function Game() {
-    return (<div>
-        
-    </div>)
+    const iBoard = [
+        ["O", "X", "-"],
+        ["O", "-", "-"],
+        ["O", "-", "X"]
+    ]
+    const boardElems = []
+
+    for (let i = 0; i < 3; i++) {
+
+        for (let i = 0; i < 3; i++) {
+            boardElems.push(<div>{i}</div>)
+            console.log(i)
+        }
+    }
+
+    return (<>
+
+        <div>
+            {/* {[(<div>1</div>), (<div>2</div>), (<div>3</div>)]} */}
+            {boardElems}
+        </div>
+
+    </>)
 }
 
 export default function GameRender() {
