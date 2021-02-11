a3165b76f9674e486a84dfa4c02300476d497ecb
pawfen <sh0n.mishel34@gmail.com>
2021-02-11 16:58:18 +0300

    static UI

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,34 +1,53 @@
+// import { StepLabel } from '@material-ui/core';
 import React from 'react';
 import { useState } from 'react'
+import styles from './Game.module.css'
 
+type GameProps = {board: string[][]}
 
+function GameRender(props:GameProps) {
+    const iBoard = props.board;
 
-function Game() {
-    const iBoard = [
-        ["O", "X", "-"],
-        ["O", "-", "-"],
-        ["O", "-", "X"]
-    ]
-    const boardElems = []
-
-    for (let i = 0; i < 3; i++) {
-
-        for (let i = 0; i < 3; i++) {
-            boardElems.push(<div>{i}</div>)
-            console.log(i)
+    const renderBoard = [] // 3 ul
+    for (let i = 0; i < iBoard.length; i++) {
+        let currentRow = iBoard[i]
+        const fillArr = [] // 3 li
+        for (let v = 0; v < currentRow.length; v++) {
+            const currentCell = currentRow[v]
+            fillArr.push(<li className={styles.li}>{(currentCell === '-') ? " " : currentCell}</li>)
         }
+        renderBoard.push(<ul className={styles.ul}>{fillArr}</ul>)
     }
 
     return (<>
-
-        <div>
+        <div className={styles.container}>
             {/* {[(<div>1</div>), (<div>2</div>), (<div>3</div>)]} */}
-            {boardElems}
+            {renderBoard}
         </div>
-
     </>)
 }
 
-export default function GameRender() {
-    return (<Game />)
+export default function Game() {
+    const iBoard2 = [
+        ["X", "X", "-", "O"],
+        ["X", "X", "X", "O"],
+        ["O", "X", "-", "X"],
+        ["X", "O", "-", "-"],
+    ]
+    const iBoard = [
+        ["X", "X", "-"],
+        ["X", "X", "X"],
+        ["O", "X", "-"]
+    ]
+
+    return (<GameRender board={iBoard} />)
 }
+
+    // for (let v = 0; v < 3; v++) {
+    //     ulElems.push((<ul style={{ display: "flex" }}><li>{v}</li></ul>))
+    //     console.log(ulElems)
+
+    //     for (let i = 0; i < 3; i++) {
+    //         ulElems.push((<li style={{ listStyle: "none" }}>X</li>))
+    //     }
+    // }
